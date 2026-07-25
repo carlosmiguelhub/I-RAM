425925248e384394720c729ec244a705605486d4
carlosmiguelhub <carlosmiguelbermejo617@gmail.com>
2026-07-25 12:18:35 +0800

    update mobile

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.94.188"],
+  allowedDevOrigins: ["192.168.1.90"],
   async rewrites() {
     return [
       {
